087834dcd23e5fdba665647ba253c04e2c61b7ad
mrt168 <kurafuto16@gmail.com>
2014-06-24 01:56:06 +0900

    click Hilight

--- src/jp/growui/meshidoco/Fragment/FragmentSelectOne.java
+++ src/jp/growui/meshidoco/Fragment/FragmentSelectOne.java
@@ -7,8 +7,10 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 
 /**
@@ -30,14 +32,28 @@ public class FragmentSelectOne extends FragmentSelects {
 	}
 
 	private ArrayList<ImageView> testImages() {
-		ArrayList<ImageView> images = new ArrayList<ImageView>();
+		final ArrayList<ImageView> images = new ArrayList<ImageView>();
 		for (int i = 0; i < 10; i++) {
 			ImageView image = new ImageView(getActivity());
+			image.setAdjustViewBounds(true);
+			image.setScaleType(ScaleType.FIT_XY);
+			image.setPadding(8, 8, 8, 8);
 			if (i == 1 || i == 4 || i == 7) {
 				image.setImageResource(R.drawable.kagemaru);
 			} else {
 				image.setImageResource(R.drawable.curry);
 			}
+			image.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View view) {
+					// TODO Auto-generated method stub
+					for (int i = 0; i < images.size(); i++) {
+						images.get(i).setBackground(null);
+					}
+					view.setBackgroundResource(R.color.red);
+				}
+			});
 			images.add(image);
 		}
 		return images;
